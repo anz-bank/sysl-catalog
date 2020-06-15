22485eb5cc37953d1280a68984136c2ffc62668f
joshcarp <joshuacarpeggiani@gmail.com>
2020-06-15 16:20:54 +1000

    Add to not fail with make

--- build.sh
+++ build.sh
@@ -5,14 +5,13 @@ git add demo/markdown || true
 git rm -rf demo/markdown/* || true
 git add docs || true
 git rm -rf docs/* || true
-make install
+make install || true
+
 sysl-catalog -o demo/markdown demo/simple2.sysl
 sysl-catalog --type=html --embed -o demo/html demo/simple2.sysl --redoc
-mkdir -p docs
-cp -r demo/html/* docs/
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  sed -i "s/simple2.sysl/<a href=http:\/\/github.com\/anz-bank\/sysl-catalog>This is an example of sysl catalog deployed to github pages <\/a>/" docs/index.html
+ sed -i "s/simple2.sysl/<a href=http:\/\/github.com\/anz-bank\/sysl-catalog>This is an example of sysl catalog deployed to github pages <\/a>/" docs/index.html
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i "" "s/simple2.sysl/<a href=http:\/\/github.com\/anz-bank\/sysl-catalog>This is an example of sysl catalog deployed to github pages <\/a>/" docs/index.html
+ sed -i "" "s/simple2.sysl/<a href=http:\/\/github.com\/anz-bank\/sysl-catalog>This is an example of sysl catalog deployed to github pages <\/a>/" docs/index.html
 fi

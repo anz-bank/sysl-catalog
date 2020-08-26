1aa01a106c5c7e487470a38101d98daaa2bc1433
Chloe Zhang <cikeyplanet@gmail.com>
2020-08-26 11:37:25 +1000

    implement github list tags and git list tags

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,22 +2,103 @@ package version
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/google/go-github/v32/github"
+	"golang.org/x/oauth2"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
 
-func GitTagsJson(org, repo string) (string, error) {
+type Tag struct {
+	Name string    `json:"name"`
+	SHA  string    `json:"sha"`
+	Date time.Time `json:"date"`
+}
+
+// ListTags return tags in order. Return up to 30 by default
+func ListTags(owner, repo string) (*[]Tag, error) {
 	client := github.NewClient(nil)
-
-	tags, _, err := client.Repositories.ListTags(context.Background(), org, repo, nil)
-	if err != nil {
-		return "", err
+	githubToken := os.Getenv("SYSL_GITHUB_TOKEN")
+	if githubToken != "" {
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: githubToken},
+		)
+		tc := oauth2.NewClient(context.Background(), ts)
+		client = github.NewClient(tc)
 	}
 
-	b, err := json.Marshal(tags[0].Commit.Author)
+	ctx := context.Background()
+
+	// rawtags, _, err := client.Repositories.ListTags(ctx, owner, repo, &github.ListOptions{PerPage: 100})
+	rawtags, _, err := client.Repositories.ListTags(ctx, owner, repo, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(b), nil
+
+	tags := make([]Tag, 0, len(rawtags))
+	for _, tag := range rawtags {
+		sha := tag.GetCommit().GetSHA()
+		rc, _, err := client.Repositories.GetCommit(ctx, owner, repo, sha)
+		if err != nil {
+			return nil, err
+		}
+
+		tags = append(tags, Tag{
+			Name: tag.GetName(),
+			SHA:  sha,
+			Date: rc.GetCommit().GetAuthor().GetDate(),
+		})
+	}
+	return &tags, nil
+}
+
+// GitListTags return disordered tags
+func GitListTags(url string) (*[]Tag, error) {
+	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: url,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	iter, err := r.Tags()
+	if err != nil {
+		return nil, err
+	}
+
+	tags := make([]Tag, 0)
+	err = iter.ForEach(func(tag *plumbing.Reference) error {
+		sha := tag.Hash().String()
+		h, err := r.ResolveRevision(plumbing.Revision(sha))
+		if err != nil {
+			return err
+		}
+		if h == nil {
+			return errors.New("revision not found")
+		}
+
+		obj, err := r.Object(plumbing.AnyObject, *h)
+		if err != nil {
+			return err
+		}
+
+		o := obj.(*object.Commit)
+
+		tags = append(tags, Tag{
+			Name: strings.TrimPrefix(tag.Name().String(), "refs/tags/"),
+			SHA:  sha,
+			Date: o.Author.When,
+		})
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &tags, nil
 }
